38f042c9d4a0a84087ac7033a5aab7d786a5f970
marumo333 <seito5026junior@icloud.com>
2025-11-07 20:34:17 +0900

    add:Googleマップをドメインに追加

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['maps.googleapis.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'maps.googleapis.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
